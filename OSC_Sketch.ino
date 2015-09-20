324abbf54f91b092277ca16eae626bdf34be9b4f
Jose Luis Garza, Jr <90joseluis@gmail.com>
2015-09-20 14:26:22 -0400

    Update OSC_Sketch.ino
    Complete mapping update to new buttons on andriod app.

--- OSC_Sketch.ino
+++ OSC_Sketch.ino
@@ -1,14 +1,14 @@
 #include <Bridge.h>
 
-const int pin0 = 3;
-const int pin1 = 4;
-const int pin2 = 5;
-const int pin3 = 6;
+const int pin2 = 2;
+const int pin3 = 3;
+const int pin4 = 4;
+const int pin5 = 5;
 
-char D10value[3];
-char D11value[3];
-char D12value[3];
-char D13value[3];
+char right1value[3];
+char right2value[3];
+char left1value[3];
+char left2value[3];
 
 int button0Value = 0;
 int button1Value = 0;
@@ -16,55 +16,57 @@ int button2Value = 0;
 int button3Value = 0;
 
 void setup() {
-  pinMode(pin0, OUTPUT);
-  pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+//  Serial.begin(9600);
   Bridge.begin();
 }
 
 
 void loop() {
-  Bridge.get("D10",D10value,3);
-  Bridge.get("D11",D11value,3);
-  Bridge.get("D12",D12value,3);
-  Bridge.get("D13",D13value,3);
-  
   //Bridge.get(key,buffer,buffer_length);
- button0Value = atoi(D10value);//A3
- button1Value = atoi(D11value);//E3
- button2Value = atoi(D12value);//A2
- button3Value = atoi(D13value);//E2
-
-if ((button0Value || button1Value || button2Value || button3Value) == 0 )
-{
-    digitalWrite(pin0, LOW);
-    digitalWrite(pin1, LOW);
-    digitalWrite(pin2, LOW);
-    digitalWrite(pin3, LOW);
+  Bridge.get("right1",right1value,3);
+  Bridge.get("right2",right2value,3);
+  Bridge.get("left1",left1value,3);
+  Bridge.get("left2",left2value,3);
   
+  
+ button0Value = atoi(right1value);
+ button1Value = atoi(right2value);
+ button2Value = atoi(left1value);
+ button3Value = atoi(left2value);
+// Serial.println(button0Value);
+
+if ((button0Value|| button1Value  || button2Value || button3Value) == 0 )
+{
+      digitalWrite(pin2, LOW);
+      digitalWrite(pin3, LOW);
+      digitalWrite(pin4, LOW);
+      digitalWrite(pin5, LOW);
 }
 
-  if (button0Value == 1)    //Left Forward A3
+  if (button0Value == 1)    //Left Forward 
   {
-    digitalWrite(pin2, LOW);
-    digitalWrite(pin3, HIGH);
+     digitalWrite(pin2, HIGH);
+     digitalWrite(pin3, LOW);
   }
  
-  if (button1Value == 1)    //Right Forward E3
+  if (button1Value == 1)    //Right Forward
   {
-    digitalWrite(pin0, LOW);
-    digitalWrite(pin1, HIGH);
+     digitalWrite(pin2, LOW);
+     digitalWrite(pin3, HIGH);
   }
-  if (button2Value == 1)     //Left Backward A2
+  if (button2Value == 1)     //Left Backward
   {
-    digitalWrite(pin2, HIGH);
-    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, HIGH);
+    digitalWrite(pin5, LOW);
   }
-  if (button3Value == 1)    //Right Backward E2
+  if (button3Value == 1)    //Right Backward
   {
-    digitalWrite(pin0, HIGH);
-    digitalWrite(pin1, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, HIGH);
   }
 
   delay(10);  
